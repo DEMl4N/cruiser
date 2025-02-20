2d3c74053aa70219b6c54a561277357eeb33b494
DEMl4N <delfirmamentos@gmail.com>
2025-02-20 12:19:00 +0900

    Fix: camera - trt conflict

--- src/camera_module.py
+++ src/camera_module.py
@@ -1,69 +1,56 @@
 import cv2
 
-class CameraModule:
-    def __init__(
-            self,
-            sensor_id=0,
-            capture_width=3264,
-            capture_height=1848,
-            display_width=816,
-            display_height=462,
-            framerate=28,
-            flip_method=0,
-    ):
-        self.sensor_id = sensor_id
-        self.capture_width = capture_width
-        self.capture_height = capture_height
-        self.display_width = display_width
-        self.display_height = display_height
-        self.framerate = framerate
-        self.flip_method = flip_method
-        self.video_capture = None
 
-        self.init_camera()
+def gstreamer_pipeline(
+        sensor_id=0,
+        capture_width=3264,
+        capture_height=1848,
+        display_width=816,
+        display_height=462,
+        framerate=28,
+        flip_method=0
+):
+    return (
+            "nvarguscamerasrc sensor-id=%d ! "
+            "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! "
+            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink"
+            % (
+                sensor_id,
+                capture_width,
+                capture_height,
+                framerate,
+                flip_method,
+                display_width,
+                display_height,
+            )
+    )
 
-    def gstreamer_pipeline(
-            self
-    ):
-        return (
-                "nvarguscamerasrc sensor-id=%d ! "
-                "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-                "nvvidconv flip-method=%d ! "
-                "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-                "videoconvert ! "
-                "video/x-raw, format=(string)BGR ! appsink"
-                % (
-                    self.sensor_id,
-                    self.capture_width,
-                    self.capture_height,
-                    self.framerate,
-                    self.flip_method,
-                    self.display_width,
-                    self.display_height,
-                )
-        )
 
-    def init_camera(self):
-        print("[+] Initializing camera...")
-        video_capture = cv2.VideoCapture(self.gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+# def init_camera(self):
+#     print("[+] Initializing camera...")
+#     video_capture = cv2.VideoCapture(self.gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+#
+#     if video_capture.isOpened():
+#         print("[+] Camera initialized successfully.")
+#         self.video_capture = video_capture
+#     else:
+#         print("[-] Failed to initialize camera.")
+#         exit()
 
-        if video_capture.isOpened():
-            print("[+] Camera initialized successfully.")
-            self.video_capture = video_capture
-        else:
-            print("[-] Failed to initialize camera.")
-            exit()
 
-    def get_frame(self):
-        frame = None
-        if self.video_capture.isOpened():
-            try:
-                _, frame = self.video_capture.read()
-
-            except Exception as e:
-                print(f"[-] Error reading frame: {e}")
-
-            finally:
-                self.video_capture.release()
-
-        return frame
+# def get_frame(self):
+#     frame = None
+#     if self.video_capture.isOpened():
+#         try:
+#             _, frame = self.video_capture.read()
+#
+#         except Exception as e:
+#             print(f"[-] Error reading frame: {e}")
+#
+#         finally:
+#             self.video_capture.release()
+#
+#     return frame
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from neural_engine import NeuralEngine
-from camera_module import CameraModule
 from in_vehicle_communication import InVehicleCommunication
 from car_state import CarState
 from lane_detector import LaneDetectionModule
 # from v2v_communication import V2VCommunication
+import camera_module
 import numpy as np
 import cv2
 
@@ -13,14 +13,13 @@ def prepare_state_modules():
     return car_state
 
 def prepare_neural_modules():
-    front_camera = CameraModule()
     lane_detector = LaneDetectionModule()
     # detection_engine = NeuralEngine("./obj_detect_v1.engine")
     # prediction_engine = NeuralEngine("./predict_v1.engine")
     control_engine = NeuralEngine("./control_v1.engine")
 
     # return front_camera, lane_detector, detection_engine, prediction_engine, control_engine
-    return front_camera, lane_detector, None, None, control_engine
+    return lane_detector, None, None, control_engine
 
 def prepare_communcation_modules(car_state: CarState):
     in_vehicle_communication = InVehicleCommunication(car_state=car_state)
@@ -30,55 +29,69 @@ def prepare_communcation_modules(car_state: CarState):
 
 def execute_hdp():
     car_state = prepare_state_modules()
-    front_camera, lane_detector, detection_engine, prediction_engine, control_engine = prepare_neural_modules()
+    lane_detector, detection_engine, prediction_engine, control_engine = prepare_neural_modules()
     in_vehicle_communication, v2v_communication = prepare_communcation_modules(car_state)
 
-    # run every 30ms
-    while True:
-        frame = front_camera.get_frame()
+    # init camera
+    pipeline = camera_module.gstreamer_pipeline()
+    print("[+] Initializing camera...")
+    front_camera = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
 
-        frame_array = np.array(frame)
-        frame_resized = cv2.resize(frame, dsize=(816, 462))
-        # todo!: lane detection
-        lane_offset = lane_detector.getLaneCurve(frame_resized, curLane=0, targetLane=0)
-        # detection_engine.infer(frame_array)
-        # lane_result = lane_detector.get_output()
-        # detection_result = detection_engine.get_output()
+    if front_camera.isOpened():
+        print("[+] Camera initialized successfully.")
+    else:
+        print("[-] Failed to initialize camera.")
+        exit()
 
-        # class_id = detection_result[0][0]
-        # box = detection_result[1][0]
-        # confidence = detection_result[2][0]
-        # prediction_engine.infer(box)
+    if front_camera.isOpened():
+        print("[+] Camera initialized successfully.")
+        print("[+] Starting HDP...")
+        # run every 30ms
+        while True:
+            frame = front_camera.get_frame()
 
-        # prediction_result = prediction_engine.get_output()
-        # front_vehicle_next_x = prediction_result[0][0]
-        front_vehicle_next_x = 0
-        # front_vehicle_next_dist = prediction_result[1][0]
-        front_vehicle_next_dist = 1
+            frame_array = np.array(frame)
+            frame_resized = cv2.resize(frame, dsize=(816, 462))
+            # todo!: lane detection
+            lane_offset = lane_detector.getLaneCurve(frame_resized, curLane=0, targetLane=0)
+            # detection_engine.infer(frame_array)
+            # lane_result = lane_detector.get_output()
+            # detection_result = detection_engine.get_output()
 
-        #lane_offset = lane_result[0][0]
+            # class_id = detection_result[0][0]
+            # box = detection_result[1][0]
+            # confidence = detection_result[2][0]
+            # prediction_engine.infer(box)
 
-        front_vehicle_x_delta = -1
-        distance_delta = 1
-        if car_state.front_vehicle_dist != 0:
-            distance_delta = min(front_vehicle_next_dist / car_state.front_vehicle_dist, 1)
+            # prediction_result = prediction_engine.get_output()
+            # front_vehicle_next_x = prediction_result[0][0]
+            front_vehicle_next_x = 0
+            # front_vehicle_next_dist = prediction_result[1][0]
+            front_vehicle_next_dist = 1
 
-        input_data = np.array([
-            front_vehicle_next_x,
-            front_vehicle_x_delta,
-            car_state.current_speed,
-            car_state.target_speed,
-            lane_offset,
-            distance_delta,
+            # lane_offset = lane_result[0][0]
+
+            front_vehicle_x_delta = -1
+            distance_delta = 1
+            if car_state.front_vehicle_dist != 0:
+                distance_delta = min(front_vehicle_next_dist / car_state.front_vehicle_dist, 1)
+
+            input_data = np.array([
+                front_vehicle_next_x,
+                front_vehicle_x_delta,
+                car_state.current_speed,
+                car_state.target_speed,
+                lane_offset,
+                distance_delta,
             ], dtype=np.float32
-        )
+            )
 
-        control_engine.infer(input_data)
-        control_result = control_engine.get_output()
-        steering_angle = control_result[0][0]
-        speed = car_state.current_speed + control_result[0][1]
+            control_engine.infer(input_data)
+            control_result = control_engine.get_output()
+            steering_angle = control_result[0][0]
+            speed = car_state.current_speed + control_result[0][1]
 
-        in_vehicle_communication.send_data(steering_angle, speed)
+            in_vehicle_communication.send_data(steering_angle, speed)
 
 
 
